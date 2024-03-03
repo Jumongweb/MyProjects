93c1cc9f5018c63ab1ddaee402779001411eeb52
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-03 20:49:43 +0100

    Pong game

---
+++ src/pongGame/GamePanel.java
@@ -0,0 +1,111 @@
+package pongGame;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+public class GamePanel extends JPanel implements Runnable{
+
+    public static final int GAME_WIDTH = 1000;
+    public static final int GAME_HEIGHT = (int)(GAME_WIDTH * (0.5555));
+    public static final Dimension SCREEN_SIZE = new Dimension(GAME_WIDTH, GAME_HEIGHT);
+    public static final int BALL_DIAMETER = 20;
+    public static final int PADDLE_WIDTH = 25;
+    public static final int PADDLE_HEIGHT = 100;
+
+    Thread gameThread;
+    Image image;
+    Graphics graphics;
+    Random random;
+    Paddle paddle1;
+    Paddle paddle2;
+    Ball ball;
+    Score score;
+
+    public GamePanel() {
+        newPaddles();
+        newBall();
+        score = new Score(GAME_WIDTH, GAME_HEIGHT);
+        this.setFocusable(true);
+        this.addKeyListener(new AL());
+        this.setPreferredSize(SCREEN_SIZE);
+
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    public void newBall() {
+
+    }
+
+    public void newPaddles(){
+        paddle1 = new Paddle(0, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 1);
+        paddle2 = new Paddle(GAME_WIDTH-PADDLE_WIDTH, (GAME_HEIGHT / 2) - (PADDLE_HEIGHT / 2), PADDLE_WIDTH, PADDLE_HEIGHT, 2);
+    }
+
+    public void paint(Graphics g){
+        image = createImage(getWidth(), getHeight());
+        graphics = image.getGraphics();
+        draw(graphics);
+        g.drawImage(image, 0,0, this);
+    }
+
+    public void draw(Graphics g) {
+        paddle1.draw(g);
+        paddle2.draw(g);
+    }
+
+    public void move(){
+
+    }
+
+    public void checkCollision(){
+
+    }
+    public void run(){
+        // game loop
+        long lastTime = System.nanoTime();
+        double amountOfTicks = 60.0;
+        double ns = 1000000000 / amountOfTicks;
+        double delta = 0;
+        while (true){
+            long now = System.nanoTime();
+            delta += (now - lastTime) /ns;
+            lastTime = now;
+            if (delta >= 1){
+                move();
+                checkCollision();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+//    public class AL extends KeyAdapter{
+//        @Override
+//        public void KeyPressed(KeyEvent e){
+//            paddle1.KeyPressed(e);
+//            paddle2.KeyPressed(e);
+//        }
+//        @Override
+//        public void KeyReleased(KeyEvent e){
+//            paddle1.KeyReleased(e);
+//            paddle2.KeyReleased(e);
+//        }
+//    }
+
+    public class AL extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            paddle1.KeyPressed(e);
+            paddle2.KeyPressed(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            paddle1.KeyReleased(e);
+            paddle2.KeyReleased(e);
+        }
+    }
+
+}
