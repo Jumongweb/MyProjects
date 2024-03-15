1a60055be5599185ed6d65a803f16e98d931b482
Lawal Toheeb <lawaltoheeb36@gmail.com>
2024-03-15 23:39:29 +0100

    snake game

--- src/snake/GamePanel.java
+++ src/snake/GamePanel.java
@@ -1,10 +1,10 @@
 package snake;
 
-
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.Random;
+import java.util.Scanner;
 
 import javax.swing.JPanel;
 
@@ -49,14 +49,22 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void draw(Graphics g) {
-
+        for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+            g.drawLine(i*UNIT_SIZE,0, i * UNIT_SIZE, SCREEN_HEIGHT);
+            g.drawLine(0,i*UNIT_SIZE,i * SCREEN_WIDTH, i*UNIT_SIZE);
+        }
+        g.setColor(Color.red);
+        g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
     }
 
     public void newApple(){
-
+        appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE)) * UNIT_SIZE;
+        appleY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE)) * UNIT_SIZE;
     }
     public void move(){
-
+        for (int i = bodyParts; i > 0; i--){
+            //x[i] = x[i-1];
+        }
     }
 
     public void checkApple() {
